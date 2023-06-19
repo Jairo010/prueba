84287aeac4c7e7c6af44e0c85972fec800cc38ef
Jairo Freire <jairofreireortiz10@gmail.com>
2023-06-19 12:18:03 -0500

    Some others tags have been added

--- index.php
+++ index.php
@@ -9,8 +9,10 @@
     <h1>Hola</h1>
     <h2>adios</h2>
     <h3>Morning</h3>
+    <h4>Good nigth</h4>
     <main>
         <p>Im happy to see you</p>
+        <p>See you soon</p>
     </main>
 </body>
 </html>
